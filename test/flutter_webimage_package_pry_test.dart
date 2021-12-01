682d576d3b64c43ed4434d57a8ab30e30be77a3f
parinda palansooriya <54696315+ParindaPalansooriya@users.noreply.github.com>
2021-12-01 19:02:06 +0530

    changers

--- test/flutter_webimage_package_pry_test.dart
+++ test/flutter_webimage_package_pry_test.dart
@@ -3,8 +3,6 @@ import 'package:flutter_webimage_package_pry/flutter_webimage_package_pry.dart';
 
 void main() {
   test('adds one to input values', () {
-    expect(WebImagePry(), "3");
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    expect(WebImagePry("https://raw.githubusercontent.com/ParindaPalansooriya/flutter_webimage_package_pry/main/Personal%20Care.png"), "https://raw.githubusercontent.com/ParindaPalansooriya/flutter_webimage_package_pry/main/Health%20Care.png");
   });
 }
